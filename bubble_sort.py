cdcc15f7bfc7920d8818b82d981020f28661110c
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-02-22 10:20:57 +0200

    Add comments with explanation how to optimize it.

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,10 +1,16 @@
 my_array = [64, 34, 25, 12, 22, 11, 90, 5]
 
 n = len(my_array)
+is_sorted = False
 
 for i in range(n-1):
-    for j in range(n-i-1):
+    is_sorted = True  # При всеки нов елемент се смята, че е сортиран, докато не се извърши swap операция на 10 ред
+    for j in range(n-i-1):  # Причината да е n-i-1, защото след всяка итерация последният елемент ще е нагласен,
+        # няма смисъл да се проверява
         if my_array[j] > my_array[j+1]:
             my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
+            is_sorted = False  # ако има swap операция значи не е все още сортиран списъкът
+    if is_sorted:  # Ако не е имало swap операция, то значи списък вече е сортиран
+        break
 
 print("Sorted array:", my_array)
