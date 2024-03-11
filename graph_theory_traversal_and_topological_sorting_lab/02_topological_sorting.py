c9847ddc33b680bb5b6720b06a95b9f6eb632b7e
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-03-11 15:03:48 +0200

    Add solution for Topological sorting

---
+++ graph_theory_traversal_and_topological_sorting_lab/02_topological_sorting.py
@@ -0,0 +1,60 @@
+def find_node_without_dependencies(elements_counter):
+    for key, value in elements_counter.items():
+        if value == 0:
+            return key
+
+    return None
+
+
+n = int(input())
+graph = {}
+elements_counter = {}
+
+for _ in range(n):
+    information = input().split('->')
+    left_element = information[0].rstrip()
+    right_element = None
+    if len(information) > 1:
+        right_element = information[1].lstrip()
+        if ',' in right_element:
+            right_element = right_element.split(', ')
+        else:
+            right_element = [right_element]
+
+    graph[left_element] = right_element
+
+    if left_element not in elements_counter:
+        elements_counter[left_element] = 0
+
+    if right_element[0] == '':
+        continue
+
+    for element in right_element:
+        if element not in elements_counter:
+            elements_counter[element] = 0
+        elements_counter[element] += 1
+
+dependencies = find_node_without_dependencies(elements_counter)
+final_list = []
+
+
+while dependencies and len(elements_counter) > 0:
+
+    node_to_remove = find_node_without_dependencies(elements_counter)
+
+    if not node_to_remove:
+        print('Invalid topological sorting')
+        exit()
+
+    for child in graph[node_to_remove]:
+        if child in elements_counter:
+            elements_counter[child] -= 1
+
+    final_list.append(node_to_remove)
+    del elements_counter[node_to_remove]
+
+if len(final_list) == 0:
+    print('Invalid topological sorting')
+    exit()
+
+print(f"Topological sorting: {', '.join(final_list)}")
